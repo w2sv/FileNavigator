cf567b20d0dadc24002892b59af6dcc8738e753c
w2sv <zangenbergjanek@googlemail.com>
2023-06-11 15:24:17 +0200

    Adjust doc

--- app/src/main/kotlin/com/w2sv/filenavigator/datastore/DataStoreVariable.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/datastore/DataStoreVariable.kt
@@ -3,7 +3,7 @@ package com.w2sv.filenavigator.datastore
 import androidx.datastore.preferences.core.Preferences
 
 /**
- * Interface for Classes, which are to be stored in a DataStore<Preferences> instance.
+ * Interface for Classes, which are to be managed as map by a [PreferencesDataStoreRepository].
  */
 interface DataStoreVariable<T> {
     val defaultValue: T
