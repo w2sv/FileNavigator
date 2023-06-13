fca91c2a59561da93748dc4fe8e63e504fb00257
w2sv <zangenbergjanek@googlemail.com>
2023-06-13 13:10:07 +0200

    Arrow right icons

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/main/FileTypeSelectionAccordion.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/main/FileTypeSelectionAccordion.kt
@@ -2,19 +2,19 @@ package com.w2sv.filenavigator.ui.screens.main
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
@@ -26,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.w2sv.filenavigator.R
 import com.w2sv.filenavigator.mediastore.FileType
 import com.w2sv.filenavigator.ui.theme.FileNavigatorTheme
 import com.w2sv.filenavigator.ui.theme.RailwayText
@@ -67,30 +68,38 @@ private fun AccordionHeader(
     Surface(tonalElevation = 2.dp, shape = RoundedCornerShape(8.dp)) {
         Row(
             modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 12.dp),
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                painter = painterResource(id = fileType.iconRes),
-                contentDescription = null,
-                modifier = Modifier.size(34.dp),
-                tint = fileType.color
-            )
-            Spacer(modifier = Modifier.width(14.dp))
-            RailwayText(
-                text = stringResource(id = fileType.titleRes),
-                fontSize = 18.sp
-            )
-            Spacer(modifier = Modifier.weight(0.8f))
-            Switch(
-                checked = mainScreenViewModel.accountForFileType.getValue(fileType),
-                onCheckedChange = {
-                    mainScreenViewModel.accountForFileType.toggle(
-                        fileType
-                    )
-                }
-            )
+            Box(modifier = Modifier.weight(0.2f), contentAlignment = Alignment.Center) {
+                Icon(
+                    painter = painterResource(id = fileType.iconRes),
+                    contentDescription = null,
+                    modifier = Modifier.size(34.dp),
+                    tint = fileType.color
+                )
+            }
+            Box(modifier = Modifier.weight(0.7f), contentAlignment = Alignment.CenterStart) {
+                RailwayText(
+                    text = stringResource(id = fileType.titleRes),
+                    fontSize = 18.sp
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .weight(0.3f)
+                    .padding(8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Switch(
+                    checked = mainScreenViewModel.accountForFileType.getValue(fileType),
+                    onCheckedChange = {
+                        mainScreenViewModel.accountForFileType.toggle(
+                            fileType
+                        )
+                    }
+                )
+            }
         }
     }
 }
@@ -132,15 +141,26 @@ private fun FileTypeOriginRow(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = modifier
                 .fillMaxWidth()
-                .padding(horizontal = 10.dp)
         ) {
-            RailwayText(text = stringResource(id = origin.kind.labelRes))
-            Checkbox(
-                checked = mainScreenViewModel.accountForFileTypeOrigin.getValue(origin),
-                onCheckedChange = {
-                    mainScreenViewModel.accountForFileTypeOrigin.toggle(origin)
-                }
-            )
+            Box(modifier = Modifier.weight(0.2f), contentAlignment = Alignment.Center) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_right_24),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary.copy(alpha = 0.4f),
+                    modifier = Modifier.size(22.dp)
+                )
+            }
+            Box(modifier = Modifier.weight(0.7f), contentAlignment = Alignment.CenterStart) {
+                RailwayText(text = stringResource(id = origin.kind.labelRes))
+            }
+            Box(modifier = Modifier.weight(0.3f), contentAlignment = Alignment.Center) {
+                Checkbox(
+                    checked = mainScreenViewModel.accountForFileTypeOrigin.getValue(origin),
+                    onCheckedChange = {
+                        mainScreenViewModel.accountForFileTypeOrigin.toggle(origin)
+                    }
+                )
+            }
         }
     }
 }
