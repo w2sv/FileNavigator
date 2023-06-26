a11afdf750c5a6bff7de3a000e975ffeba4ca370
w2sv <zangenbergjanek@googlemail.com>
2023-06-26 14:15:31 +0200

    Refactor

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/Buttons.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/Buttons.kt
@@ -1,12 +1,23 @@
 package com.w2sv.filenavigator.ui
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -31,4 +42,33 @@ fun DialogButton(
         ),
         content = content
     )
+}
+
+private enum class ButtonState { Pressed, Idle }
+
+fun Modifier.bounceOnClickAnimation(minScale: Float = 0.8f) = composed {
+    var buttonState by remember { mutableStateOf(ButtonState.Idle) }
+    val scale by animateFloatAsState(
+        if (buttonState == ButtonState.Pressed) minScale else 1f,
+        label = ""
+    )
+
+    this
+        .scale(scale)
+//        .clickable(
+//            interactionSource = remember { MutableInteractionSource() },
+//            indication = null,
+//            onClick = { }
+//        )
+        .pointerInput(buttonState) {
+            awaitPointerEventScope {
+                buttonState = if (buttonState == ButtonState.Pressed) {
+                    waitForUpOrCancellation()
+                    ButtonState.Idle
+                } else {
+                    awaitFirstDown(false)
+                    ButtonState.Pressed
+                }
+            }
+        }
 }
--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/main/StartNavigatorButton.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/main/StartNavigatorButton.kt
@@ -7,14 +7,11 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.with
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,16 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -51,6 +44,7 @@ import com.w2sv.filenavigator.datastore.PreferencesKey
 import com.w2sv.filenavigator.navigator.service.FileNavigatorService
 import com.w2sv.filenavigator.ui.AppFontText
 import com.w2sv.filenavigator.ui.ExtendedSnackbarVisuals
+import com.w2sv.filenavigator.ui.bounceOnClickAnimation
 import com.w2sv.filenavigator.ui.showSnackbarAndDismissCurrentIfApplicable
 import com.w2sv.filenavigator.ui.theme.DefaultAnimationDuration
 import com.w2sv.filenavigator.ui.theme.disabledColor
@@ -68,7 +62,7 @@ private data class NavigatorButtonProperties(
 )
 
 @OptIn(
-    ExperimentalPermissionsApi::class, ExperimentalFoundationApi::class,
+    ExperimentalPermissionsApi::class,
     ExperimentalAnimationApi::class
 )
 @Composable
@@ -173,7 +167,7 @@ internal fun StartNavigatorButton(
 
     ElevatedButton(
         onClick = properties.onClick,
-        modifier = modifier.bounceClick(),
+        modifier = modifier.bounceOnClickAnimation(),
         enabled = anyStorageAccessGranted
     ) {
         AnimatedContent(
@@ -222,36 +216,4 @@ private fun ButtonContent(properties: NavigatorButtonProperties, anyStorageAcces
             color = if (anyStorageAccessGranted) MaterialTheme.colorScheme.onBackground else disabledColor()
         )
     }
-}
-
-enum class ButtonState { Pressed, Idle }
-
-fun Modifier.bounceClick(minScale: Float = 0.8f) = composed {
-    var buttonState by remember { mutableStateOf(ButtonState.Idle) }
-    val scale by animateFloatAsState(
-        if (buttonState == ButtonState.Pressed) minScale else 1f,
-        label = ""
-    )
-
-    this
-        .graphicsLayer {
-            scaleX = scale
-            scaleY = scale
-        }
-//        .clickable(
-//            interactionSource = remember { MutableInteractionSource() },
-//            indication = null,
-//            onClick = { }
-//        )
-        .pointerInput(buttonState) {
-            awaitPointerEventScope {
-                buttonState = if (buttonState == ButtonState.Pressed) {
-                    waitForUpOrCancellation()
-                    ButtonState.Idle
-                } else {
-                    awaitFirstDown(false)
-                    ButtonState.Pressed
-                }
-            }
-        }
 }
