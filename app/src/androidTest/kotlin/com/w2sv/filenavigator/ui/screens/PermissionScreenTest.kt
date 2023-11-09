82a5258084da5094bdea499a58be2aa42c8d34c9
w2sv <zangenbergjanek@googlemail.com>
2023-11-09 17:04:47 +0100

    Do stuff

---
+++ app/src/androidTest/kotlin/com/w2sv/filenavigator/ui/screens/PermissionScreenTest.kt
@@ -0,0 +1,25 @@
+package com.w2sv.filenavigator.ui.screens
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.w2sv.filenavigator.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+class PermissionScreenTest {
+
+    @get:Rule
+    val composeContentTestRule: ComposeContentTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testNavigationDrawerScreen() {
+        with(composeContentTestRule) {
+            waitForIdle()
+
+            onNodeWithText("Missing Permissions")
+                .assertIsDisplayed()
+        }
+    }
+}
--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/missingpermissions/MissingPermissionsScreen.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/missingpermissions/MissingPermissionsScreen.kt
@@ -3,19 +3,12 @@ package com.w2sv.filenavigator.ui.screens.missingpermissions
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.w2sv.filenavigator.R
-import com.w2sv.filenavigator.ui.components.AppFontText
 import com.w2sv.filenavigator.ui.theme.DefaultAnimationDuration
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -24,31 +17,20 @@ fun PermissionScreen(
     properties: List<PermissionCardProperties>,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
+    LazyColumn(
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
     ) {
-        Spacer(modifier = Modifier.fillMaxHeight(0.1f))
-        AppFontText(
-            text = stringResource(id = R.string.permissions_missing),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Medium,
-            modifier = Modifier.align(Alignment.Start)
-        )
-        LazyColumn(
-            verticalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxHeight(0.85f)
-        ) {
-            items(properties, key = { it.hashCode() }) {
-                PermissionCard(
-                    properties = it,
-                    modifier = Modifier.animateItemPlacement(
-                        tween(
-                            DefaultAnimationDuration
-                        )
+        items(properties, key = { it.hashCode() }) {
+            PermissionCard(
+                properties = it,
+                modifier = Modifier.animateItemPlacement(
+                    tween(
+                        DefaultAnimationDuration
                     )
                 )
-            }
+            )
         }
     }
 }
