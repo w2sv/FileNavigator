014bc4f6be55b30813e25d1ba7fbf0d504c710f6
w2sv <zangenbergjanek@googlemail.com>
2024-03-15 12:34:51 +0100

    Update guava, triplet-play

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/missingpermissions/MissingPermissionsScreen.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/missingpermissions/MissingPermissionsScreen.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.movableContentOf
@@ -13,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -52,7 +57,11 @@ fun PermissionScreen(
             modifier = Modifier.fillMaxSize()
         ) {
             permissionCards.forEach {
-                it(Modifier.fillMaxWidth(0.4f))
+                it(
+                    Modifier
+                        .fillMaxWidth(0.4f)
+                        .verticalScroll(rememberScrollState())
+                )
             }
         }
     }
