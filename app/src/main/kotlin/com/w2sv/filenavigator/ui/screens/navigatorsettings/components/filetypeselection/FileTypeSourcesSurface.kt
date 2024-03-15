6f059dd2ca5f93da26df5fc9543357df936f2586
w2sv <zangenbergjanek@googlemail.com>
2024-03-15 12:51:42 +0100

    Use InterElementDividedColumn from composed:0.0.3-rc2

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/navigatorsettings/components/filetypeselection/FileTypeSourcesSurface.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/navigatorsettings/components/filetypeselection/FileTypeSourcesSurface.kt
@@ -16,10 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.w2sv.composed.InterElementDividedColumn
 import com.w2sv.domain.model.FileType
 import com.w2sv.filenavigator.ui.designsystem.AppCheckbox
 import com.w2sv.filenavigator.ui.model.color
-import com.w2sv.filenavigator.ui.utils.ElementDividedColumn
 import com.w2sv.filenavigator.ui.utils.orDisabledIf
 import kotlinx.collections.immutable.toImmutableList
 
@@ -35,7 +35,7 @@ fun FileTypeSourcesSurface(
         tonalElevation = 8.dp,
         modifier = modifier.fillMaxWidth()
     ) {
-        ElementDividedColumn(
+        InterElementDividedColumn(
             elements = fileType.sources.toImmutableList(),
             makeElement = { source ->
                 SourceRow(
