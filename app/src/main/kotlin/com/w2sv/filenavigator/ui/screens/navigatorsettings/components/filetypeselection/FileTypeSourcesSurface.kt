9e67cd33b1a1cbaef8bbfaf2198d65c024fc6c24
w2sv <zangenbergjanek@googlemail.com>
2024-05-14 16:58:07 +0200

    Fix checkbox color

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/navigatorsettings/components/filetypeselection/FileTypeSourcesSurface.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/screens/navigatorsettings/components/filetypeselection/FileTypeSourcesSurface.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -81,7 +82,7 @@ private fun SourceRow(
 
         Box(modifier = Modifier.weight(0.15f), contentAlignment = Alignment.Center) {
             if (source.fileType.isMediaType) {
-                AppCheckbox(
+                Checkbox(
                     checked = isEnabled,
                     onCheckedChange = onCheckedChange
                 )
