637de2f09e16b1b82bdf5760f44701d28f5ee689
w2sv <zangenbergjanek@googlemail.com>
2024-07-19 17:59:17 +0200

    Polish

--- core/navigator/src/main/kotlin/com/w2sv/navigator/notifications/managers/abstrct/SummarizedMultiInstanceNotificationManager.kt
+++ core/navigator/src/main/kotlin/com/w2sv/navigator/notifications/managers/abstrct/SummarizedMultiInstanceNotificationManager.kt
@@ -8,6 +8,9 @@ import com.w2sv.navigator.notifications.AppNotificationChannel
 import com.w2sv.navigator.notifications.AppNotificationId
 import com.w2sv.navigator.notifications.NotificationResources
 
+/**
+ * Notification manager for multi-instance notifications that post a summary.
+ */
 internal abstract class SummarizedMultiInstanceNotificationManager<A : MultiInstanceNotificationManager.BuilderArgs>(
     appNotificationChannel: AppNotificationChannel,
     notificationManager: NotificationManager,
@@ -21,12 +24,10 @@ internal abstract class SummarizedMultiInstanceNotificationManager<A : MultiInst
 ) {
     private val summaryNotificationId = appNotificationId.id + 999  // TODO
 
-    private fun emitSummaryNotification() {
-        notificationManager.notify(summaryNotificationId, buildSummaryNotification())
-    }
-
-    abstract fun buildSummaryNotification(): Notification?
-
+    /**
+     * Implicitly cancels or updates the summary notification depending on the number of active
+     * notifications after cancelling the one specified by [resources].
+     */
     @CallSuper
     override fun cancelNotificationAndFreeResources(resources: NotificationResources) {
         super.cancelNotificationAndFreeResources(resources)
@@ -35,16 +36,26 @@ internal abstract class SummarizedMultiInstanceNotificationManager<A : MultiInst
             notificationManager.cancel(summaryNotificationId)
         } else {
             // Update notification
-            emitSummaryNotification()
+            postSummaryNotification()
         }
     }
 
+    /**
+     * Implicitly posts the summary notification if there are at least 2 active notifications after
+     * posting the one specified by [args].
+     */
     @CallSuper
     override fun buildAndPost(args: A) {
         super.buildAndPost(args)
 
         if (activeNotificationCount >= 2) {
-            emitSummaryNotification()
+            postSummaryNotification()
         }
     }
+
+    private fun postSummaryNotification() {
+        notificationManager.notify(summaryNotificationId, buildSummaryNotification())
+    }
+
+    abstract fun buildSummaryNotification(): Notification
 }
