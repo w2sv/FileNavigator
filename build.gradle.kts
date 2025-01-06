d6edde65dc19f0cab485a41df336fc67ed0faa1e
w2sv <zangenbergjanek@googlemail.com>
2025-01-06 17:21:49 +0100

    Add .editorconfig | Revise gradle.properties | Add ktlint and versionCatalogUpdate

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -7,4 +9,26 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.play) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.versions)
+    alias(libs.plugins.versionCatalogUpdate)
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
+
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+// Taken from https://github.com/ben-manes/gradle-versions-plugin#rejectversionsif-and-componentselection
+private fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
