d4ea0ac1ec4bc4fd5238f825e289c8f7d23ae655
w2sv <zangenbergjanek@googlemail.com>
2025-02-17 16:49:57 +0100

    Refine AMOLED theme

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -36,67 +37,66 @@ fun AppTheme(
             else -> rememberDynamicColorScheme(
                 seedColor = seedColor,
                 isDark = useDarkTheme,
-                isAmoled = false, // TODO
+                isAmoled = false,
                 style = PaletteStyle.Rainbow // Vibrant, TonalSpot, Fidelity, Rainbow
             )
         }
             .run {
                 if (useAmoledBlackTheme && useDarkTheme) {
-                    copy(background = Color.Black, surface = Color.Black)
+                    copy(background = Color.Black, surface = Color.Black, onBackground = Color.White, onSurface = Color.White)
                 } else {
                     this
                 }
-            }
-            .animate(animationSpec = remember { spring(stiffness = Spring.StiffnessMedium) }),
+            },
         typography = typography
     ) {
         content()
     }
 }
 
-@Composable
-private fun ColorScheme.animate(animationSpec: AnimationSpec<Color>): ColorScheme {
-    return copy(
-        primary = primary.animate(animationSpec),
-        primaryContainer = primaryContainer.animate(animationSpec),
-        secondary = secondary.animate(animationSpec),
-        secondaryContainer = secondaryContainer.animate(animationSpec),
-        tertiary = tertiary.animate(animationSpec),
-        tertiaryContainer = tertiaryContainer.animate(animationSpec),
-        background = background.animate(animationSpec),
-        surface = surface.animate(animationSpec),
-        surfaceTint = surfaceTint.animate(animationSpec),
-        surfaceBright = surfaceBright.animate(animationSpec),
-        surfaceDim = surfaceDim.animate(animationSpec),
-        surfaceContainer = surfaceContainer.animate(animationSpec),
-        surfaceContainerHigh = surfaceContainerHigh.animate(animationSpec),
-        surfaceContainerHighest = surfaceContainerHighest.animate(animationSpec),
-        surfaceContainerLow = surfaceContainerLow.animate(animationSpec),
-        surfaceContainerLowest = surfaceContainerLowest.animate(animationSpec),
-        surfaceVariant = surfaceVariant.animate(animationSpec),
-        error = error.animate(animationSpec),
-        errorContainer = errorContainer.animate(animationSpec),
-        onPrimary = onPrimary.animate(animationSpec),
-        onPrimaryContainer = onPrimaryContainer.animate(animationSpec),
-        onSecondary = onSecondary.animate(animationSpec),
-        onSecondaryContainer = onSecondaryContainer.animate(animationSpec),
-        onTertiary = onTertiary.animate(animationSpec),
-        onTertiaryContainer = onTertiaryContainer.animate(animationSpec),
-        onBackground = onBackground.animate(animationSpec),
-        onSurface = onSurface.animate(animationSpec),
-        onSurfaceVariant = onSurfaceVariant.animate(animationSpec),
-        onError = onError.animate(animationSpec),
-        onErrorContainer = onErrorContainer.animate(animationSpec),
-        inversePrimary = inversePrimary.animate(animationSpec),
-        inverseSurface = inverseSurface.animate(animationSpec),
-        inverseOnSurface = inverseOnSurface.animate(animationSpec),
-        outline = outline.animate(animationSpec),
-        outlineVariant = outlineVariant.animate(animationSpec),
-        scrim = scrim.animate(animationSpec)
-    )
-}
-
-@Composable
-private fun Color.animate(animationSpec: AnimationSpec<Color>): Color {
-    return animateColorAsState(this, animationSpec, label = "").value
-}
+//@Composable
+//private fun ColorScheme.animate(animationSpec: AnimationSpec<Color>): ColorScheme {
+//    return copy(
+//        primary = primary.animate(animationSpec),
+//        primaryContainer = primaryContainer.animate(animationSpec),
+//        secondary = secondary.animate(animationSpec),
+//        secondaryContainer = secondaryContainer.animate(animationSpec),
+//        tertiary = tertiary.animate(animationSpec),
+//        tertiaryContainer = tertiaryContainer.animate(animationSpec),
+//        background = background.animate(animationSpec),
+//        surface = surface.animate(animationSpec),
+//        surfaceTint = surfaceTint.animate(animationSpec),
+//        surfaceBright = surfaceBright.animate(animationSpec),
+//        surfaceDim = surfaceDim.animate(animationSpec),
+//        surfaceContainer = surfaceContainer.animate(animationSpec),
+//        surfaceContainerHigh = surfaceContainerHigh.animate(animationSpec),
+//        surfaceContainerHighest = surfaceContainerHighest.animate(animationSpec),
+//        surfaceContainerLow = surfaceContainerLow.animate(animationSpec),
+//        surfaceContainerLowest = surfaceContainerLowest.animate(animationSpec),
+//        surfaceVariant = surfaceVariant.animate(animationSpec),
+//        error = error.animate(animationSpec),
+//        errorContainer = errorContainer.animate(animationSpec),
+//        onPrimary = onPrimary.animate(animationSpec),
+//        onPrimaryContainer = onPrimaryContainer.animate(animationSpec),
+//        onSecondary = onSecondary.animate(animationSpec),
+//        onSecondaryContainer = onSecondaryContainer.animate(animationSpec),
+//        onTertiary = onTertiary.animate(animationSpec),
+//        onTertiaryContainer = onTertiaryContainer.animate(animationSpec),
+//        onBackground = onBackground.animate(animationSpec),
+//        onSurface = onSurface.animate(animationSpec),
+//        onSurfaceVariant = onSurfaceVariant.animate(animationSpec),
+//        onError = onError.animate(animationSpec),
+//        onErrorContainer = onErrorContainer.animate(animationSpec),
+//        inversePrimary = inversePrimary.animate(animationSpec),
+//        inverseSurface = inverseSurface.animate(animationSpec),
+//        inverseOnSurface = inverseOnSurface.animate(animationSpec),
+//        outline = outline.animate(animationSpec),
+//        outlineVariant = outlineVariant.animate(animationSpec),
+//        scrim = scrim.animate(animationSpec)
+//    )
+//}
+//
+//@Composable
+//private fun Color.animate(animationSpec: AnimationSpec<Color>): Color {
+//    return animateColorAsState(this, animationSpec, label = "").value
+//}
