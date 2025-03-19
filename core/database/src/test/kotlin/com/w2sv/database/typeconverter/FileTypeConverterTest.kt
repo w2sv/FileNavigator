d02c10939ebbdd73a9525d947dfa881171c27034
w2sv <zangenbergjanek@googlemail.com>
2025-03-19 04:45:23 +0100

    Add testBackAndForthCustomFileTypeConversion test

--- core/database/src/test/kotlin/com/w2sv/database/typeconverter/FileTypeConverterTest.kt
+++ core/database/src/test/kotlin/com/w2sv/database/typeconverter/FileTypeConverterTest.kt
@@ -1,5 +1,6 @@
 package com.w2sv.database.typeconverter
 
+import com.w2sv.domain.model.CustomFileType
 import com.w2sv.domain.model.PresetFileType
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
@@ -7,9 +8,21 @@ import org.junit.Test
 internal class FileTypeConverterTest {
 
     @Test
-    fun testBackAndForthFileTypeConversion() {
+    fun testBackAndForthPresetFileTypeConversion() {
         PresetFileType.values.forEach {
             assertEquals(it, FileTypeConverter.toFileType(FileTypeConverter.fromFileType(it)))
         }
+    }
+
+    @Test
+    fun testBackAndForthCustomFileTypeConversion() {
+
+        fun test(customFileType: CustomFileType) {
+            val recreatedFileType = FileTypeConverter.toFileType(FileTypeConverter.fromFileType(customFileType)) as CustomFileType
+            assertEquals(customFileType.name, recreatedFileType.name)
+            assertEquals(customFileType.colorInt, recreatedFileType.colorInt)
+        }
+
+        test(CustomFileType("Html", emptyList(), 342523, -1))
     }
 }
