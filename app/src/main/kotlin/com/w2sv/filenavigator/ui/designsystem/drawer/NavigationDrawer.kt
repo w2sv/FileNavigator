1da097faf539ad878c5fe574fb292de73655cadd
w2sv <zangenbergjanek@googlemail.com>
2025-05-20 22:46:35 +0200

    Remove strings from resources that should never be translated | Remove unused Chinese plurals quantities

--- app/src/main/kotlin/com/w2sv/filenavigator/ui/designsystem/drawer/NavigationDrawer.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/designsystem/drawer/NavigationDrawer.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -84,7 +85,7 @@ private fun Header(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.SemiBold
         )
         Spacer(modifier = Modifier.height(12.dp))
-        Text(text = stringResource(R.string.copyright, LocalDate.now().year))
+        Text(text = remember { "© 2023 - ${LocalDate.now().year} | w2sv" })
     }
 }
 
