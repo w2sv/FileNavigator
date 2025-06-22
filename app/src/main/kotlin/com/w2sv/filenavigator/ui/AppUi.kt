2bf5902ea0bf1b236dd4171a27062c2bae3f7a4f
w2sv <zangenbergjanek@googlemail.com>
2025-06-22 23:51:13 +0200

    AppUi composable

---
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/AppUi.kt
@@ -0,0 +1,71 @@
+package com.w2sv.filenavigator.ui
+
+import android.graphics.Color
+import androidx.activity.SystemBarStyle
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.w2sv.domain.model.Theme
+import com.w2sv.domain.usecase.MoveDestinationPathConverter
+import com.w2sv.filenavigator.ui.navigation.NavGraph
+import com.w2sv.filenavigator.ui.state.rememberPostNotificationsPermissionState
+import com.w2sv.filenavigator.ui.theme.AppTheme
+import com.w2sv.filenavigator.ui.util.activityViewModel
+
+@Composable
+fun AppUi(
+    setSystemBarStyles: (SystemBarStyle, SystemBarStyle) -> Unit,
+    triggerStatusBarStyleUpdate: Boolean,
+    moveDestinationPathConverter: MoveDestinationPathConverter,
+    appVM: AppViewModel = activityViewModel()
+) {
+    val theme by appVM.theme.collectAsStateWithLifecycle()
+    val useDarkTheme = useDarkTheme(theme = theme)
+    val useAmoledBlackTheme by appVM.useAmoledBlackTheme.collectAsStateWithLifecycle()
+    val useDynamicColors by appVM.useDynamicColors.collectAsStateWithLifecycle()
+    val anyPermissionMissing by appVM.permissions.anyMissing.collectAsStateWithLifecycle()
+
+    CompositionLocalProvider(
+        LocalUseDarkTheme provides useDarkTheme,
+        LocalMoveDestinationPathConverter provides moveDestinationPathConverter,
+        LocalPostNotificationsPermissionState provides rememberPostNotificationsPermissionState(
+            onPermissionResult = { appVM.permissions.savePostNotificationsRequested() },
+            onStatusChanged = appVM.permissions::setPostNotificationsGranted
+        )
+    ) {
+        AppTheme(
+            useDarkTheme = useDarkTheme,
+            useAmoledBlackTheme = useAmoledBlackTheme,
+            useDynamicColors = useDynamicColors
+        ) {
+            // Reset system bar styles on theme change
+            LaunchedEffect(useDarkTheme, triggerStatusBarStyleUpdate) {
+                val systemBarStyle = if (useDarkTheme) {
+                    SystemBarStyle.dark(Color.TRANSPARENT)
+                } else {
+                    SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                }
+
+                setSystemBarStyles(
+                    systemBarStyle,
+                    systemBarStyle
+                )
+            }
+
+            NavGraph(anyPermissionMissing = anyPermissionMissing)
+        }
+    }
+}
+
+@ReadOnlyComposable
+@Composable
+private fun useDarkTheme(theme: Theme): Boolean =
+    when (theme) {
+        Theme.Light -> false
+        Theme.Dark -> true
+        Theme.Default -> isSystemInDarkTheme()
+    }
--- app/src/main/kotlin/com/w2sv/filenavigator/ui/MainActivity.kt
+++ app/src/main/kotlin/com/w2sv/filenavigator/ui/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.w2sv.filenavigator.ui
 
 import android.animation.ObjectAnimator
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -22,22 +15,17 @@ import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.splashscreen.SplashScreenViewProvider
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.w2sv.domain.model.Theme
 import com.w2sv.domain.usecase.MoveDestinationPathConverter
-import com.w2sv.filenavigator.ui.navigation.NavGraph
-import com.w2sv.filenavigator.ui.state.rememberPostNotificationsPermissionState
-import com.w2sv.filenavigator.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val appVM by viewModels<AppViewModel>()
-
     @Inject
     lateinit var moveDestinationPathConverter: MoveDestinationPathConverter
+
+    private val appVM by viewModels<AppViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var triggerStatusBarStyleUpdate by mutableStateOf(false)
@@ -51,43 +39,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val theme by appVM.theme.collectAsStateWithLifecycle()
-            val useDarkTheme = useDarkTheme(theme = theme)
-            val useAmoledBlackTheme by appVM.useAmoledBlackTheme.collectAsStateWithLifecycle()
-            val useDynamicColors by appVM.useDynamicColors.collectAsStateWithLifecycle()
-            val anyPermissionMissing by appVM.permissions.anyMissing.collectAsStateWithLifecycle()
-
-            CompositionLocalProvider(LocalUseDarkTheme provides useDarkTheme) {
-                AppTheme(
-                    useDarkTheme = useDarkTheme,
-                    useAmoledBlackTheme = useAmoledBlackTheme,
-                    useDynamicColors = useDynamicColors
-                ) {
-                    // Reset system bar styles on theme change
-                    LaunchedEffect(useDarkTheme, triggerStatusBarStyleUpdate) {
-                        val systemBarStyle = if (useDarkTheme) {
-                            SystemBarStyle.dark(Color.TRANSPARENT)
-                        } else {
-                            SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
-                        }
-
-                        enableEdgeToEdge(
-                            systemBarStyle,
-                            systemBarStyle
-                        )
-                    }
-
-                    CompositionLocalProvider(
-                        LocalMoveDestinationPathConverter provides moveDestinationPathConverter,
-                        LocalPostNotificationsPermissionState provides rememberPostNotificationsPermissionState(
-                            onPermissionResult = { appVM.permissions.savePostNotificationsRequested() },
-                            onStatusChanged = appVM.permissions::setPostNotificationsGranted
-                        )
-                    ) {
-                        NavGraph(anyPermissionMissing = anyPermissionMissing)
-                    }
-                }
-            }
+            AppUi(
+                setSystemBarStyles = ::enableEdgeToEdge,
+                triggerStatusBarStyleUpdate = triggerStatusBarStyleUpdate,
+                moveDestinationPathConverter = moveDestinationPathConverter
+            )
         }
     }
 
@@ -96,15 +52,6 @@ class MainActivity : ComponentActivity() {
         appVM.permissions.updateManageAllFilesPermission()
     }
 }
-
-@ReadOnlyComposable
-@Composable
-private fun useDarkTheme(theme: Theme): Boolean =
-    when (theme) {
-        Theme.Light -> false
-        Theme.Dark -> true
-        Theme.Default -> isSystemInDarkTheme()
-    }
 
 private class SwipeRightSplashScreenExitAnimation(private val onAnimationEnd: () -> Unit = {}) :
     SplashScreen.OnExitAnimationListener {
