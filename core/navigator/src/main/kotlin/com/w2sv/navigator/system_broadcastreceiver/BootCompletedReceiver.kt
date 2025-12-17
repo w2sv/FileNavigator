b9f6925500e181d8889a7fc2c282408c86b9fd7d
w2sv <zangenbergjanek@googlemail.com>
2025-12-17 22:43:05 +0100

    fix: navigator always being started on boot completed

--- core/navigator/src/main/kotlin/com/w2sv/navigator/system_broadcastreceiver/BootCompletedReceiver.kt
+++ core/navigator/src/main/kotlin/com/w2sv/navigator/system_broadcastreceiver/BootCompletedReceiver.kt
@@ -1,15 +1,44 @@
 package com.w2sv.navigator.system_broadcastreceiver
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import com.w2sv.common.di.AppDispatcher
+import com.w2sv.common.di.GlobalScope
+import com.w2sv.common.util.LoggingBroadcastReceiver
+import com.w2sv.domain.repository.NavigatorConfigDataSource
 import com.w2sv.navigator.FileNavigator
-import slimber.log.i
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
-internal class BootCompletedReceiver : SystemBroadcastReceiver(Intent.ACTION_BOOT_COMPLETED) {
+/**
+ * Called on system emission of [Intent.ACTION_BOOT_COMPLETED].
+ * Starts the [FileNavigator] if [com.w2sv.domain.model.navigatorconfig.NavigatorConfig.startOnBoot] true and the necessary permissions for
+ * running the navigator are granted.
+ *
+ * Doesn't require any registering via [Context.registerReceiver], as BOOT_COMPLETED receivers are automatically registered via the manifest.
+ */
+@AndroidEntryPoint
+internal class BootCompletedReceiver : LoggingBroadcastReceiver() {
 
-    override fun onReceiveMatchingIntent(context: Context, intent: Intent) {
-        i { "BootCompletedReceiver.onReceive" }
+    @Inject
+    lateinit var navigatorConfigDataSource: NavigatorConfigDataSource
 
-        FileNavigator.start(context)
+    @Inject
+    @GlobalScope(AppDispatcher.Default)
+    internal lateinit var scope: CoroutineScope
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        scope.launch {
+            if (navigatorConfigDataSource.navigatorConfig.first().startOnBoot && FileNavigator.necessaryPermissionsGranted(context)) {
+                FileNavigator.start(context)
+            }
+        }
     }
 }
--- core/navigator/src/main/kotlin/com/w2sv/navigator/system_broadcastreceiver/manager/SystemBroadcastReceiverManager.kt
+++
@@ -1,8 +0,0 @@
-package com.w2sv.navigator.system_broadcastreceiver.manager
-
-import android.content.Context
-import kotlinx.coroutines.CoroutineScope
-
-interface SystemBroadcastReceiverManager {
-    fun launchReceiverTogglingOnNavigatorConfigChange(flowCollectionScope: CoroutineScope, context: Context)
-}
