3d2c9f4b27c0e3f7b07cc6a89b71aba8e62847c0
w2sv <zangenbergjanek@googlemail.com>
2026-01-11 14:58:43 +0100

    fix: batch move notification channel

--- core/navigator-notifications/src/main/kotlin/com/w2sv/navigator/notifications/AppNotification.kt
+++ core/navigator-notifications/src/main/kotlin/com/w2sv/navigator/notifications/AppNotification.kt
@@ -6,8 +6,8 @@ package com.w2sv.navigator.notifications
 internal enum class AppNotification(val channel: AppNotificationChannel) {
     FileNavigatorIsRunning(AppNotificationChannel.FileNavigatorIsRunning),
     NewNavigatableFile(AppNotificationChannel.NewNavigatableFile),
+    BatchMoveFiles(AppNotificationChannel.NewNavigatableFile),
     AutoMoveDestinationInvalid(AppNotificationChannel.AutoMoveDestinationInvalid),
-    BatchMoveFiles(AppNotificationChannel.MoveProgress),
     BatchMoveProgress(AppNotificationChannel.MoveProgress);
 
     val id: Int get() = ordinal + 1 // 0 is an invalid notification ID
