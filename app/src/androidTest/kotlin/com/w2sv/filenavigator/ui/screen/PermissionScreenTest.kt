a07d1e99dfaba59f1868d2067d537700522d65db
w2sv <zangenbergjanek@googlemail.com>
2026-06-04 22:01:27 +0200

    test: fix PermissionScreenTest import

--- app/src/androidTest/kotlin/com/w2sv/filenavigator/ui/screen/PermissionScreenTest.kt
+++ app/src/androidTest/kotlin/com/w2sv/filenavigator/ui/screen/PermissionScreenTest.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import com.w2sv.filenavigator.MainActivity
+import com.w2sv.filenavigator.ui.MainActivity
 import org.junit.Rule
 import org.junit.Test
 
