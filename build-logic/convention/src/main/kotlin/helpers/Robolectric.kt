f092b5855bc3d35c1aea31e4a933e2f3e7522113
w2sv <zangenbergjanek@googlemail.com>
2026-06-06 13:44:31 +0200

    build: attempt to increase build speed

--- build-logic/convention/src/main/kotlin/helpers/Robolectric.kt
+++ build-logic/convention/src/main/kotlin/helpers/Robolectric.kt
@@ -3,20 +3,15 @@ package helpers
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import java.io.File
 
 /**
- * There is no global way to set the Robolectric SDK version, so we generate robolectric.properties
- * in the module's build/generated/testResources.
- *
- * Adopted from https://github.com/PaulWoitaschek/Voice/blob/main/plugins/src/main/kotlin/configureRobolectricSdk.kt
+ * There is no global way to set the Robolectric SDK version, so each module uses the shared
+ * robolectric.properties as a test resource.
  */
 fun setRobolectricSdk(target: Project) {
-    val generatedResourceDir = target.file("build/generated/testResources").apply { mkdirs() }
-    File(generatedResourceDir, "robolectric.properties").apply { writeText("sdk=35") }
     target.extensions.configure<CommonExtension> {
         sourceSets.named("test") {
-            resources.directories.add(generatedResourceDir.path)
+            resources.directories.add(target.rootProject.file("config/robolectric").path)
         }
     }
 }
