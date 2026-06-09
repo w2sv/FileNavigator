1bfb8104c0ff4e5f4fff07a63f4090f5c47b938b
w2sv <zangenbergjanek@googlemail.com>
2026-06-09 10:09:42 +0200

    fix: move file notification large icon tints

--- modules/navigator-notifications/src/main/kotlin/com/w2sv/navigator/notifications/helper/Shared.kt
+++ modules/navigator-notifications/src/main/kotlin/com/w2sv/navigator/notifications/helper/Shared.kt
@@ -2,16 +2,22 @@ package com.w2sv.navigator.notifications.helper
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.toBitmap
+import com.w2sv.androidutils.res.isNightModeActiveCompat
 import com.w2sv.domain.model.filetype.FileAndSourceType
 
 internal fun FileAndSourceType.iconBitmap(context: Context, colored: Boolean = false): Bitmap? =
     context.drawableBitmap(
         drawable = iconRes,
-        tint = if (colored) fileType.colorInt else null
+        tint = when {
+            colored -> fileType.colorInt
+            context.resources.configuration.isNightModeActiveCompat -> null
+            else -> Color.BLACK
+        }
     )
 
 internal fun Context.drawableBitmap(@DrawableRes drawable: Int, @ColorInt tint: Int? = null): Bitmap? =
